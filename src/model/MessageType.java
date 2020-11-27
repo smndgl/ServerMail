9a297b03dc7e4e695906ca850cbbf48768abf7af
Simone Dogliani <simone.dogliani585@edu.unito.it>
2020-11-27 09:32:10 +0100

    added two type of delete message to difersify from which mailbox user is removing email

--- src/model/MessageType.java
+++ src/model/MessageType.java
@@ -1,9 +1,11 @@
 package model;
 
 public enum  MessageType {
-    send,
-    delete,
-    sync,
-    login,
-    fetch
+    send, //client sends an email
+    delete_i, //client deletes an email from inbox or sent
+    delete_s,
+    sync,  //server sends sync msg for advise client of a new email
+    login, //username check and login
+    logout, //remove mailboxes from runtime memory of server
+    fetch //mailboxes retrieving from thread Sync
 }
